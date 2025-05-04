756c01a19d8c4830578d87305d7d1cadbd8d65a5
omidshabab <omidshababdev@gmail.com>
2025-05-04 08:38:37 +0100

    Enhance language support by adding Farsi and English fonts, improving language detection, and refining input handling

---
+++ test_custom_input/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import CustomInputDemo from "@/components/ui/demo/CustomInputDemo";
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-background p-4">
+      <CustomInputDemo />
+    </div>
+  );
+};
+
+export default Page;
